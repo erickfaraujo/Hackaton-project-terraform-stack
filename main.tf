1fc887880ac0127e14a39fd382b5919b39e16869
Erick <erick.faraujo@gmail.com>
2021-05-22 14:33:59 +0000

    ajuste exercicio

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ variable "project" {
 
 data "aws_vpc" "vpc" {
   tags = {
-    Name = "${var.project}-${terraform.workspace}"
+    Name = "${var.project}"
   }
 }
 
@@ -42,7 +42,7 @@ resource "random_shuffle" "random_subnet" {
 
 
 resource "aws_elb" "web" {
-  name = "hackton-elb-${terraform.workspace}"
+  name = "hackton-elb"
 
   subnets         = data.aws_subnet_ids.all.ids
   security_groups = ["${aws_security_group.allow-ssh.id}"]
@@ -96,6 +96,6 @@ resource "aws_instance" "web" {
   }
 
   tags = {
-    Name = "${format("nginx-hackaton-%03d", count.index + 1)}-${terraform.workspace}"
+    Name = "${format("nginx-hackaton-%03d", count.index + 1)}"
   }
 }
